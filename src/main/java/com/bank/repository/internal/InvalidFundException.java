cf90a66ef09402b6f329f9c2801bf1e3d36a278f
Twin Panichsombat <roofimon@gmail.com>
2013-02-06 11:26:22 +0700

    Add git ignore

---
+++ src/main/java/com/bank/repository/internal/InvalidFundException.java
@@ -0,0 +1,11 @@
+package com.bank.repository.internal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: roofimon
+ * Date: 2/6/13 AD
+ * Time: 10:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InvalidFundException {
+}
