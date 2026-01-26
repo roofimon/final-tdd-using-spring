611136739fe78244bf2550b989c18953c2493f4a
RuuF <ruuf@odds.team>
2026-01-26 12:27:48 +0700

    Switch tests to MatcherAssert.assertThat

--- src/test/java/com/bank/config/xml/IntegrationITCase.java
+++ src/test/java/com/bank/config/xml/IntegrationITCase.java
@@ -16,7 +16,7 @@
 package com.bank.config.xml;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -27,22 +27,22 @@ import com.bank.service.TransferService;
 
 public class IntegrationITCase {
 
-    @Test
-    public void transferTenDollars() throws InsufficientFundsException {
-        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        //ctx.getEnvironment().setActiveProfiles("dev");
-        ctx.load("classpath:transfer-service-config.xml");
-        ctx.refresh();
+	@Test
+	public void transferTenDollars() throws InsufficientFundsException {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		// ctx.getEnvironment().setActiveProfiles("dev");
+		ctx.load("classpath:transfer-service-config.xml");
+		ctx.refresh();
 
-        TransferService transferService = ctx.getBean(TransferService.class);
-        AccountRepository accountRepository = ctx.getBean(AccountRepository.class);
+		TransferService transferService = ctx.getBean(TransferService.class);
+		AccountRepository accountRepository = ctx.getBean(AccountRepository.class);
 
-        assertThat(accountRepository.findById("A123").getBalance(), equalTo(100.00));
-        assertThat(accountRepository.findById("C456").getBalance(), equalTo(0.00));
+		assertThat(accountRepository.findById("A123").getBalance(), equalTo(100.00));
+		assertThat(accountRepository.findById("C456").getBalance(), equalTo(0.00));
 
-        transferService.transfer(10.00, "A123", "C456");
+		transferService.transfer(10.00, "A123", "C456");
 
-        assertThat(accountRepository.findById("A123").getBalance(), equalTo(90.00));
-        assertThat(accountRepository.findById("C456").getBalance(), equalTo(10.00));
-    }
+		assertThat(accountRepository.findById("A123").getBalance(), equalTo(90.00));
+		assertThat(accountRepository.findById("C456").getBalance(), equalTo(10.00));
+	}
 }
--- src/test/java/com/bank/service/internal/VariableFeePolicyTest.java
+++ src/test/java/com/bank/service/internal/VariableFeePolicyTest.java
@@ -1,21 +1,21 @@
 package com.bank.service.internal;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
 
 public class VariableFeePolicyTest {
-	
+
 	@Test
 	public void testVariableFeePolicy() {
 		VariableFeePolicy feePolicy = new VariableFeePolicy(1000d, 1000000d, 1d, 20000d);
-		//1,000,001 up flat rate 2,0000
+		// 1,000,001 up flat rate 2,0000
 		assertThat(feePolicy.calculateFee(1000001d), equalTo(20000d));
-		//1,001 - 1,000,000 percent 1%
+		// 1,001 - 1,000,000 percent 1%
 		assertThat(feePolicy.calculateFee(1000000d), equalTo(10000d));
 		assertThat(feePolicy.calculateFee(1001d), equalTo(10.01d));
-		//1000 down free 0
+		// 1000 down free 0
 		assertThat(feePolicy.calculateFee(1000d), equalTo(0d));
 	}
 
